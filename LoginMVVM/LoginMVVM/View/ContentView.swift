1d3d165d05ad3442359f27cf6fbdb8e858b7feaa
Stefan Bayne <stefanbayne@gmail.com>
2022-11-16 08:51:24 -0500

    Finished demo for login.

--- LoginMVVM/LoginMVVM/View/ContentView.swift
+++ LoginMVVM/LoginMVVM/View/ContentView.swift
@@ -7,30 +7,154 @@
 
 import SwiftUI
 
+/*
+ A sample login page with a custom color palette.
+ 
+ Their is also an alert dialog box example.
+ */
 struct ContentView: View {
     
     @StateObject var viewModel = ViewModel()
+    @State private var showAlert = false
     
     var body: some View {
         
-        // if user is authenticated go to next screen
-        if !viewModel.authenticated {
+        /*
+         Checks wether or not the user is aunthenticated.
+         Displays the screen based on the boolean variable.
+         */
+        if viewModel.authenticated {
             ZStack {
-                Color("Pink").ignoresSafeArea()
+                
+                Color("Pink").opacity(0.8).ignoresSafeArea() // bg
+                
                 VStack(alignment: .center, spacing: 40) {
-                    Image(systemName: "hourglass")
-                        .foregroundColor(Color("LightBlue")
-                }
-            }
-        } else {
-            // else show login screen
-            ZStack {
-                Color("DarkerBlue").ignoresSafeArea(.all, edges: .all)
-                VStack {
                     
+                    Spacer()
+                    
+                    Text("Login Successful!")
+                        .font(.largeTitle)
+                        .bold()
+                        .foregroundColor(Color("DarkerBlue"))
+                    
+                    Image(systemName: "lock.open.desktopcomputer")
+                        .resizable()
+                        .renderingMode(.original)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 105, height: 105)
+                        .foregroundColor(Color("DarkerBlue"))
+                    
+                    Text("Welcome back \(viewModel.username.uppercased())!")
+                        .font(.title)
+                        .bold()
+                        .foregroundColor(Color("DarkerBlue"))
+                    
+                    Spacer()
+                    
+                    Button("Back to Login Screen", action: viewModel.logOut)
+                        .frame(width: 320, height: 70)
+                        .font(.system(size: 26))
+                        .foregroundColor(Color("Pink"))
+                        .background(Color("DarkerBlue"))
+                        .cornerRadius(10)
+                        .padding(.bottom, 30)
+                        .shadow(radius: 5)
                 }
                 .padding()
             }
+        } else {
+            
+            /*
+             If user is validated, this stack shows.
+             */
+            ZStack {
+                Color("DarkerBlue").ignoresSafeArea(.all, edges: .all) // bg
+                
+                VStack(spacing: 20) {
+                    Text("Welcome!")
+                        .font(.system(size: 65))
+                        .bold()
+                        .foregroundColor(Color("Pink"))
+                        .padding(.top, 60)
+                        .textCase(.uppercase)
+                        .shadow(radius: 5)
+                    
+                    Spacer()
+                    
+                    VStack(alignment: .leading, spacing: 20) {
+                        
+                        Text("Begin...")
+                            .font(.system(size: 45))
+                            .bold()
+                            .foregroundColor(Color("Pink"))
+                            .shadow(radius: 5)
+                        
+                        TextField("Username", text: $viewModel.username)
+                            .font(.system(size: 20))
+                            .textFieldStyle(.roundedBorder)
+                            .foregroundColor(Color("Pink"))
+                            .font(.headline)
+                            .cornerRadius(10)
+                            .shadow(radius: 5)
+                            .textInputAutocapitalization(.never)
+                        
+                        SecureField("Password", text: $viewModel.password)
+                            .font(.system(size: 20))
+                            .foregroundColor(Color("Pink"))
+                            .textFieldStyle(.roundedBorder)
+                            .cornerRadius(10)
+                            .shadow(radius: 5)
+                            .textInputAutocapitalization(.never)
+                            .privacySensitive() // very important
+                        
+                        HStack {
+                            Spacer()
+                            
+                            Button("Forgot Password") {
+                                showAlert = true
+                            }
+                            .font(.system(size: 20))
+                            .bold()
+                            .foregroundColor(Color("Pink"))
+                            .cornerRadius(10)
+                            .shadow(radius: 10)
+                            
+                            Spacer()
+                            
+                        } // alert dialog box demo below
+                        .alert(isPresented: $showAlert) {
+                            Alert(
+                                title: Text("Forgot Password:"),
+                                message: Text("A link has been sent to your email."),
+                                primaryButton: .default(
+                                    Text("Resend"),
+                                    action: viewModel.buttonsNotUsed
+                                ),
+                                secondaryButton: .destructive(
+                                    Text("Dismiss"),
+                                    action: viewModel.buttonsNotUsed
+                                )
+                            )
+                        }
+                    }
+                    
+                    Spacer()
+                    
+                    Button("Login", action: viewModel.authenticate)
+                        .frame(width: 320, height: 70)
+                        .font(.system(size: 30))
+                        .foregroundColor(Color("DarkerBlue"))
+                        .background(Color("Pink"))
+                        .cornerRadius(10)
+                        .shadow(radius: 5)
+                        .padding(.bottom, 30)
+                }
+                .padding()
+                .alert("Access Denied", isPresented: $viewModel.invalid) {
+                    Button("Dismiss", action: viewModel.buttonsNotUsed)
+                }
+            } // animation. The best way to do it!
+            .transition(.scale)
         }
     }
 }
--- LoginMVVM/LoginMVVM/ViewModel/ContentViewModel.swift
+++ LoginMVVM/LoginMVVM/ViewModel/ContentViewModel.swift
@@ -8,72 +8,72 @@
 import Foundation
 import SwiftUI
 
-extension ContentView {
-    class ViewModel: ObservableObject {
-        
-        @AppStorage("AUTH_KEY") var authenticated = false {// key to be stored by userdefaults
-            willSet {// tells program object will change - same as @Published annotation
-                objectWillChange.send()
-            }
-        }
-        
-        @AppStorage("USER_KEY") var username = "username" // key for program remembers username upon logout
-        @Published var password = "password"
-        @Published var invalid: Bool = false
-        
-        /*
-         What the user has to type in to be validated.
-         */
-        private var sampleUser = "username"
-        private var samplePassword = "password"
-        
-        /*
-         Init block for when the view model is first created.
-         */
-        init() {
-            print("Currently logged on: \(authenticated)")
-            print("Current User: \(username)")
-        }
-        
-        /*
-         Method to toggle the authentication.
-         */
-        func adjustAuthentication() {
-            self.password = ""
-            withAnimation {
-                authenticated.toggle() // turns to true and false
-            }
-        }
-        
-        /*
-         Method to check if username and password is correct.
-         */
-        func authenticate() {
-            guard self.username.lowercased() == sampleUser else {
-                self.invalid == true
-                return
-            }
-            
-            guard self.password.lowercased() == samplePassword else {
-                self.invalid == true
-                return
-            }
-            // toggle accordingly to information
-            adjustAuthentication()
-        }
-        
-        /*
-         What the toggle does when logged out.
-         */
-        func logOut() {
-            adjustAuthentication()
-        }
-        
-        /*
-         For the buttons that don't do anything for this example.
-         */
-        func buttonsNotUsed() {
-            print("Add some code!")
+
+class ViewModel: ObservableObject {
+    
+    @AppStorage("AUTH_KEY") var authenticated = false {// key to be stored by userdefaults
+        willSet {// tells program object will change - same as @Published annotation
+            objectWillChange.send()
         }
     }
+    
+    @AppStorage("USER_KEY") var username = "" // key for program remembers username upon logout
+    @Published var password = ""
+    @Published var invalid: Bool = false
+    
+    /*
+     What the user has to type in to be validated.
+     */
+    private var sampleUser = "username"
+    private var samplePassword = "password"
+    
+    /*
+     Init block for when the view model is first created.
+     */
+    init() {
+        print("Currently logged on: \(authenticated)")
+        print("Current User: \(username)")
+    }
+    
+    /*
+     Method to toggle the authentication.
+     */
+    func adjustAuthentication() {
+        self.password = ""
+        withAnimation {
+            authenticated.toggle() // turns to true and false
+        }
+    }
+    
+    /*
+     Method to check if username and password is correct.
+     */
+    func authenticate() {
+        guard self.username.lowercased() == sampleUser else {
+            self.invalid = true
+            return
+        }
+        
+        guard self.password.lowercased() == samplePassword else {
+            self.invalid = true
+            return
+        }
+        // toggle accordingly to information
+        adjustAuthentication()
+    }
+    
+    /*
+     What the toggle does when logged out.
+     */
+    func logOut() {
+        adjustAuthentication()
+    }
+    
+    /*
+     For the buttons that don't do anything for this example.
+     */
+    func buttonsNotUsed() {
+        print("Add some code!")
+    }
 }
+
